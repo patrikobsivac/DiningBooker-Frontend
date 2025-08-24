a13b94ec10ae0b3742bfc811920e4526f8614915
Patrik Tovernić Obšivač <patriktovernic@gmail.com>
2025-08-24 21:50:32 +0200

    Update Register i login
    - Dodana funkcionalnost prijave i registracije korisnika
    - Integriran frontend i backend za login i registraciju

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,177 +1,169 @@
 <template>
-	<v-container fill-height fluid>
-		<v-row align="center" justify="center">
-			<v-col align="center" justify="center" cols="12">
-				<v-card class="custom-card" width="500px" outlined>
-					<v-card-title align="left" class="primary--text">Prijava</v-card-title>
-					<v-card-subtitle align="left">
-						Unesite podatke za prijavu
-					</v-card-subtitle>
-					<v-card-text class="custom-card-text">
-						<v-form v-model="isValid">
-							<v-text-field
-								v-model="userEmail"
-								dense
-								label="E-mail"
-								clearable
-								type="email"
-								:rules="[fieldRules.required, fieldRules.email]"
-								outlined></v-text-field>
-							<v-text-field
-								v-model="userPassword"
-								dense
-								label="Lozinka"
-								clearable
-								:append-icon="togglePassword ? 'mdi-eye' : 'mdi-eye-off'"
-								:rules="[fieldRules.required, fieldRules.minLength]"
-								:type="togglePassword ? 'text' : 'password'"
-								@click:append="togglePasswordVisibility"
-								outlined></v-text-field>
-						</v-form>
-						<v-btn
-							@click="showPasswordDialog"
-							class="text-left"
-							text
-							small
-							color="blue lighten-2">
-							Zaboravljena lozinka?
-						</v-btn>
-						<v-btn
-							outlined
-							small
-							color="teal darken-1"
-							to="/registracija">
-							Niste registrirani? Kliknite ovdje
-						</v-btn>
-					</v-card-text>
-					<v-card-actions class="custom-card-actions">
-						<v-btn
-							@click="submitLogin"
-							:disabled="disableLoginButton"
-							outlined
-							color="primary">
-							Potvrdi
-						</v-btn>
-					</v-card-actions>
-				</v-card>
-				<v-dialog
-					width="350px"
-					persistent
-					v-model="passwordResetDialog">
-					<v-card>
-						<v-card-title>Resetiraj Lozinku</v-card-title>
-						<v-card-subtitle>
-							Unesite svoj e-mail za resetiranje lozinke
-						</v-card-subtitle>
-						<v-card-text>
-							<v-text-field
-								v-model="emailForPasswordReset"
-								dense
-								label="E-mail"
-								clearable
-								type="text"
-								:rules="[fieldRules.required, fieldRules.email]"
-								outlined></v-text-field>
-						</v-card-text>
-						<v-card-actions>
-							<v-btn
-								color="red"
-								outlined
-								text
-								small
-								@click="closePasswordDialog">
-								Zatvori
-							</v-btn>
-							<v-btn
-								outlined
-								small
-								color="green darken-2"
-								@click="sendResetLink(emailForPasswordReset)">
-								Pošalji
-							</v-btn>
-						</v-card-actions>
-					</v-card>
-				</v-dialog>
-			</v-col>
-		</v-row>
-	</v-container>
+  <v-container fill-height fluid>
+    <v-row align="center" justify="center">
+      <v-col align="center" justify="center" cols="12">
+        <v-card class="custom-card" width="500px" outlined>
+          <v-card-title align="left" class="primary--text">Prijava</v-card-title>
+          <v-card-subtitle align="left">
+            Unesite podatke za prijavu
+          </v-card-subtitle>
+          <v-card-text class="custom-card-text">
+            <v-form v-model="isValid">
+              <v-text-field
+                v-model="userEmail"
+                dense
+                label="E-mail"
+                clearable
+                type="email"
+                :rules="[fieldRules.required, fieldRules.email]"
+                outlined
+              ></v-text-field>
+              <v-text-field
+                v-model="userPassword"
+                dense
+                label="Lozinka"
+                clearable
+                :append-icon="togglePassword ? 'mdi-eye' : 'mdi-eye-off'"
+                :rules="[fieldRules.required, fieldRules.minLength]"
+                :type="togglePassword ? 'text' : 'password'"
+                @click:append="togglePasswordVisibility"
+                outlined
+              ></v-text-field>
+            </v-form>
+            <v-btn
+              @click="showPasswordDialog"
+              class="text-left"
+              text
+              small
+              color="blue lighten-2"
+            >
+              Zaboravljena lozinka?
+            </v-btn>
+            <v-btn
+              outlined
+              small
+              color="teal darken-1"
+              to="/registracija"
+            >
+              Niste registrirani? Kliknite ovdje
+            </v-btn>
+          </v-card-text>
+          <v-card-actions class="custom-card-actions">
+            <v-btn
+              @click="submitLogin"
+              :disabled="disableLoginButton"
+              outlined
+              color="primary"
+            >
+              Potvrdi
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+
+        <!-- Dialog za reset lozinke -->
+        <v-dialog width="350px" persistent v-model="passwordResetDialog">
+          <v-card>
+            <v-card-title>Resetiraj Lozinku</v-card-title>
+            <v-card-subtitle>
+              Unesite svoj e-mail za resetiranje lozinke
+            </v-card-subtitle>
+            <v-card-text>
+              <v-text-field
+                v-model="emailForPasswordReset"
+                dense
+                label="E-mail"
+                clearable
+                type="text"
+                :rules="[fieldRules.required, fieldRules.email]"
+                outlined
+              ></v-text-field>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn color="red" outlined text small @click="closePasswordDialog">
+                Zatvori
+              </v-btn>
+              <v-btn
+                outlined
+                small
+                color="green darken-2"
+                @click="sendResetLink(emailForPasswordReset)"
+              >
+                Pošalji
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 import axios from "axios";
 export default {
-	name: 'LoginPage',
-	data() {
-		return {
-			emailForPasswordReset: null,
-			passwordResetDialog: false,
-			disableLoginButton: false,
-			isValid: true,
-			userEmail: null,
-			userPassword: null,
-			togglePassword: false,
-			fieldRules: {
-				required: (value) => !!value || "Ovo polje je obavezno",
-				minLength: (v) => v?.length >= 6 || "Lozinka mora imati najmanje 6 znakova",
-				email: (v) =>
-					!v ||
-					/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) ||
-					"Neispravna e-mail adresa!",
-			},
-		};
-	},
-	methods: {
-		async submitLogin() {
-			if (!this.isValid) return;
+  name: "LoginPage",
+  data() {
+    return {
+      emailForPasswordReset: null,
+      passwordResetDialog: false,
+      disableLoginButton: false,
+      isValid: true,
+      userEmail: null,
+      userPassword: null,
+      togglePassword: false,
+      fieldRules: {
+        required: (value) => !!value || "Ovo polje je obavezno",
+        minLength: (v) => v?.length >= 4 || "Lozinka mora imati najmanje 4 znakova",
+        email: (v) =>
+          !v ||
+          /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) ||
+          "Neispravna e-mail adresa!",
+      },
+    };
+  },
+  methods: {
+    async submitLogin() {
+      if (!this.isValid) return;
 
-			try {
-				const response = await axios.post("#", {
-					email: this.userEmail,
-					password: this.userPassword
-				});
+      try {
+        const response = await axios.post("http://localhost:3000/login", {
+          email: this.userEmail,
+          password: this.userPassword,
+        });
 
-				if (response.data.success) {
-					localStorage.setItem("authToken", response.data.token);
-					this.$toast.success("Prijava uspješna!");
-					this.$router.push("/"); 
-				} else {
-					this.$toast.error(response.data.message || "Neuspješna prijava!");
-				}
-			} catch (error) {
-				console.error(error);
-				this.$toast.error("Greška pri prijavi!");
-			}
-		},
-		async sendResetLink(email) {
-			try {
-				const response = await axios.post("#", { email });
-
-				if (response.data.success) {
-					this.$toast.success("Link za reset lozinke poslan!");
-				} else {
-					this.$toast.error(response.data.message || "Neuspješan reset!");
-				}
-			} catch (error) {
-				console.error(error);
-				this.$toast.error("Greška pri resetu lozinke!");
-			}
-			this.closePasswordDialog();
-		},
-		togglePasswordVisibility() {
-			this.togglePassword = !this.togglePassword;
-		},
-		closePasswordDialog() {
-			this.passwordResetDialog = false;
-		},
-		showPasswordDialog() {
-			this.passwordResetDialog = true;
-		},
-	},
-	watch: {
-		isValid(newVal) {
-			this.disableLoginButton = !newVal;
-		},
-	},
+        localStorage.setItem("authToken", response.data.token);
+        this.$toast.success("Prijava uspješna!");
+        this.$router.push("/");
+      } catch (error) {
+        console.error(error);
+        this.$toast.error(error.response?.data?.error || "Greška pri prijavi!");
+      }
+    },
+    async sendResetLink(email) {
+      try {
+        await axios.post("http://localhost:3000/changePass", { email });
+        this.$toast.success("Link za reset lozinke poslan!");
+      } catch (error) {
+        console.error(error);
+        this.$toast.error("Greška pri resetu lozinke!");
+      }
+      this.closePasswordDialog();
+    },
+    togglePasswordVisibility() {
+      this.togglePassword = !this.togglePassword;
+    },
+    closePasswordDialog() {
+      this.passwordResetDialog = false;
+    },
+    showPasswordDialog() {
+      this.passwordResetDialog = true;
+    },
+  },
+  watch: {
+    isValid(newVal) {
+      this.disableLoginButton = !newVal;
+    },
+  },
 };
 </script>
 
@@ -182,24 +174,19 @@ export default {
   backdrop-filter: blur(6px);
   box-shadow: 0 8px 20px rgba(0, 0, 0, 0.2);
 }
-
 .primary--text {
-	color: #1e88e5 !important;
+  color: #1e88e5 !important;
 }
-
 .custom-card-text {
-	padding-top: 20px;
+  padding-top: 20px;
 }
-
 .custom-card-actions {
-	justify-content: flex-end;
+  justify-content: flex-end;
 }
-
 .text-left {
-	margin-left: 0;
+  margin-left: 0;
 }
-
 .text-right {
-	margin-right: 0;
+  margin-right: 0;
 }
 </style>
--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,109 +1,135 @@
 <template>
-	<v-container fill-height fluid>
-		<v-row align="center" justify="center">
-			<v-col align="center" justify="center" cols="12">
-				<v-card class="custom-card" width="400px" outlined>
-					<v-card-title align="left" class="primary--text">Registracija</v-card-title>
-					<v-card-subtitle align="left">
-						Unesite podatke za registraciju
-					</v-card-subtitle>
-					<v-card-text class="custom-card-text">
-						<v-form v-model="valid">
-							<v-text-field
-								v-model="email"
-								dense
-								label="E-mail"
-								clearable
-								type="email"
-								:rules="[rules.required, rules.email]"
-								outlined></v-text-field>
-							<v-text-field
-								v-model="password"
-								dense
-								label="Zaporka"
-								clearable
-								:append-icon="showIcon ? 'mdi-eye' : 'mdi-eye-off'"
-								:rules="[rules.required, rules.min]"
-								:type="showIcon ? 'text' : 'password'"
-								@click:append="togglePasswordVisibility"
-								outlined></v-text-field>
-						</v-form>
-					</v-card-text>
-					<v-card-actions class="custom-card-actions">
-						<v-btn
-							color="red darken-3"
-							outlined
-							@click="clearFormData">
-							Očisti
-						</v-btn>
-						<v-btn
-							:disabled="isButtonDisabled"
-							outlined
-							color="primary"
-							@click="registerUser">
-							Potvrdi
-						</v-btn>
-					</v-card-actions>
-				</v-card>
-			</v-col>
-		</v-row>
-	</v-container>
+  <v-container fill-height fluid>
+    <v-row align="center" justify="center">
+      <v-col align="center" justify="center" cols="12">
+        <v-card class="custom-card" width="400px" outlined>
+          <v-card-title align="left" class="primary--text">Registracija</v-card-title>
+          <v-card-subtitle align="left">
+            Unesite podatke za registraciju
+          </v-card-subtitle>
+          <v-card-text class="custom-card-text">
+            <v-form v-model="valid">
+              <v-text-field
+                v-model="firstName"
+                dense
+                label="Ime"
+                clearable
+                :rules="[rules.required]"
+                outlined
+              ></v-text-field>
+              <v-text-field
+                v-model="lastName"
+                dense
+                label="Prezime"
+                clearable
+                :rules="[rules.required]"
+                outlined
+              ></v-text-field>
+              <v-text-field
+                v-model="phoneNumber"
+                dense
+                label="Broj mobitela"
+                clearable
+                :rules="[rules.required]"
+                outlined
+              ></v-text-field>
+              <v-text-field
+                v-model="email"
+                dense
+                label="E-mail"
+                clearable
+                type="email"
+                :rules="[rules.required, rules.email]"
+                outlined
+              ></v-text-field>
+              <v-text-field
+                v-model="password"
+                dense
+                label="Zaporka"
+                clearable
+                :append-icon="showIcon ? 'mdi-eye' : 'mdi-eye-off'"
+                :rules="[rules.required, rules.min]"
+                :type="showIcon ? 'text' : 'password'"
+                @click:append="togglePasswordVisibility"
+                outlined
+              ></v-text-field>
+            </v-form>
+          </v-card-text>
+          <v-card-actions class="custom-card-actions">
+            <v-btn color="red darken-3" outlined @click="clearFormData">
+              Očisti
+            </v-btn>
+            <v-btn :disabled="isButtonDisabled" outlined color="primary" @click="registerUser">
+              Potvrdi
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 import axios from "axios";
-export default {
-	name: "RegisterView",
-	data() {
-		return {
-			isButtonDisabled: false,
-			valid: true,
-			email: null,
-			password: null,
-			showIcon: false,
-			rules: {
-				required: (value) => !!value || "Ovo polje je obavezno",
-				min: (v) => v?.length >= 6 || "Zaporka mora imati najmanje 6 znakova!",
-				email: (v) =>
-					!v ||
-					/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) ||
-					"E-mail mora biti valjan",
-			},
-		};
-	},
-	watch: {
-		valid(newVal) {
-			this.isButtonDisabled = !newVal;
-		},
-	},
-	methods: {
-		clearFormData() {
-			this.email = null;
-			this.password = null;
-		},
-async registerUser() {
-  if (!this.valid) return;
-  try {
-    const response = await axios.post("#", {
-      email: this.email,
-      password: this.password
-    });
 
-    if (response.data.success) {
-      this.$toast.success("Registracija uspješna! Prijavite se.");
-      this.$router.push("/login");
-    } else {
-      this.$toast.error(response.data.message || "Došlo je do greške");
-    }
-  } catch (error) {
-    console.error(error);
-    this.$toast.error("Greška pri registraciji!");
-  }
-},
-		togglePasswordVisibility() {
-			this.showIcon = !this.showIcon;
-		},
-	},
+export default {
+  name: "RegisterView",
+  data() {
+    return {
+      isButtonDisabled: false,
+      valid: true,
+      firstName: null,
+      lastName: null,
+      phoneNumber: null,
+      email: null,
+      password: null,
+      showIcon: false,
+      rules: {
+        required: (value) => !!value || "Ovo polje je obavezno",
+        min: (v) => v?.length >= 4 || "Zaporka mora imati najmanje 4 znakova!",
+        email: (v) =>
+          !v ||
+          /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) ||
+          "E-mail mora biti valjan",
+      },
+    };
+  },
+  watch: {
+    valid(newVal) {
+      this.isButtonDisabled = !newVal;
+    },
+  },
+  methods: {
+    clearFormData() {
+      this.firstName = null;
+      this.lastName = null;
+      this.phoneNumber = null;
+      this.email = null;
+      this.password = null;
+    },
+    async registerUser() {
+      if (!this.valid) return;
+
+      try {
+        await axios.post("http://localhost:3000/register", {
+          firstName: this.firstName,
+          lastName: this.lastName,
+          phoneNumber: this.phoneNumber,
+          email: this.email,
+          password: this.password,
+        });
+
+        this.$toast.success("Registracija uspješna! Prijavite se.");
+        this.$router.push("/login");
+      } catch (error) {
+        console.error(error);
+        this.$toast.error(error.response?.data?.error || "Greška pri registraciji!");
+      }
+    },
+    togglePasswordVisibility() {
+      this.showIcon = !this.showIcon;
+    },
+  },
 };
 </script>
 
@@ -114,24 +140,19 @@ async registerUser() {
   backdrop-filter: blur(6px);
   box-shadow: 0 8px 20px rgba(0, 0, 0, 0.2);
 }
-
 .primary--text {
-	color: #1e88e5 !important;
+  color: #1e88e5 !important;
 }
-
 .custom-card-text {
-	padding-top: 20px;
+  padding-top: 20px;
 }
-
 .custom-card-actions {
-	justify-content: flex-end;
+  justify-content: flex-end;
 }
-
 .text-left {
-	margin-left: 0;
+  margin-left: 0;
 }
-
 .text-right {
-	margin-right: 0;
+  margin-right: 0;
 }
 </style>
